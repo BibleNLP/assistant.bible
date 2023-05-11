99b4d636a341f6a64fb8e51d945eb1523201eb26
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2023-05-11 15:45:54 +0530

    Use environment variables for chorma db path and collection name

--- app/main.py
+++ app/main.py
@@ -13,6 +13,8 @@ from log_configs import log
 import chromadb
 from chromadb.config import Settings
 
+CHROMA_DB_PATH = os.environ.get("CHROMA_DB_PATH", "../chromadb")
+CHROMA_DB_COLLECTION = os.environ.get("CHROMA_DB_COLLECTION", "a_dot_b_collection")
 
 app = FastAPI(title="Assistant.Bible  APIs", version="0.0.1-alpha.1",
     description="The server application that provides APIs to interact \
@@ -37,7 +39,7 @@ async def startup_event():
     # This method connects to the DB that get stored on the server itself where the app is running
     chroma_client = chromadb.Client(Settings(
         chroma_db_impl='duckdb+parquet',
-        persist_directory="../chromadb"))
+        persist_directory=CHROMA_DB_PATH))
 
     # Option 2
     # This method requires us to run the chroma DB as a separate service (say in docker-compose).
@@ -49,12 +51,13 @@ async def startup_event():
     #         chroma_server_http_port="8000"
     #     ))
     # Also need to sort out the following
+    # * Let the connection details, host, port and collection name be passed in requests
     # * how authentication for chorma DB access will work in that case
     # * how the connection can be handled like session that is started upon each user's API request
     #   to let each user connect to the DB that he prefers and has rights for(fastapi's Depends())
 
     collection = chroma_client.get_or_create_collection(
-        name="adotb_collection",
+        name=CHROMA_DB_COLLECTION,
         # avoid having to create embeddings in two steps before storing and querying
         # embedding_function=custom_openai_emb_fn,
         )
