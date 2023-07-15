a4521001facd2cd1be3f918b03d35cbc51c27652
Mark <woodwardmw@gmail.com>
2023-07-14 18:00:47 -0700

    Add Supabase Auth and Whisper

--- app/core/auth/__init__.py
+++ app/core/auth/__init__.py
@@ -1,6 +1,10 @@
 '''Functions for Authentication for the Apps'''
 from functools import wraps
 from custom_exceptions import PermissionException
+import os
+
+from supabase import create_client, Client
+
 
 def auth_check_decorator(func):
     '''checks a predefined token in request header'''
@@ -12,7 +16,21 @@ def auth_check_decorator(func):
         # Raises 403 error for unauthorized access
         # Passes the list of accessible sources to the calling function
 
-        ## Until Supabase or something else is ready
+        # url: str = os.environ.get("SUPABASE_URL")
+        # key: str = os.environ.get("SUPABASE_KEY")
+        # supabase: Client = create_client(url, key)
+
+        # email: str = kwargs.get('email')
+        # password: str = kwargs.get('password')
+        # email = 'woodwardmw@gmail.com'
+        # password = 'password'
+        # try:
+        #     data = supabase.auth.sign_in_with_password({ "email": email, "password": password})
+        # except Exception as e:
+        #     raise PermissionException("Cannot authenticate user in Supabase")
+        # session = supabase.auth.get_session(data['access_token'])
+
+        # Until Supabase or something else is ready
         token = kwargs.get('token')
         if token != "chatchatchat":
             raise PermissionException("Wrong access token!!!")
