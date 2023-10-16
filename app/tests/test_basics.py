52e3f3d687ea5ab75234a626579860e934d5e36f
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2023-10-16 18:56:23 +0530

    Update tests for UI page loading

--- app/tests/test_basics.py
+++ app/tests/test_basics.py
@@ -1,4 +1,4 @@
-'''Basic tests to check App is working'''
+'''Basic tests to check App is working and pages are loading'''
 from . import client
 
 def test_test_endpoint():
@@ -14,5 +14,10 @@ def test_index_page_load():
 
 def test_chat_demo_page_load():
     '''test the default API endpoints and check if App is up'''
-    response = client.get("/ui")
+    response = client.get("/app")
+    assert response.status_code == 200
+
+def test_login_page_load():
+    '''test the default API endpoints and check if App is up'''
+    response = client.get("/login")
     assert response.status_code == 200
