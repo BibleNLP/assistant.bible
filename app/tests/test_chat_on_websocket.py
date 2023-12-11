9d6b5f13e5cb959c2c444b4ad2da78a6f7539fcf
Kavitha.Raju <kavitha.raju@bridgeconn.com>
2023-12-11 12:29:51 +0530

    Fix linting issues

--- app/tests/test_chat_on_websocket.py
+++ app/tests/test_chat_on_websocket.py
@@ -2,8 +2,8 @@
 import os
 import json
 import pytest
-from . import client
 from app import schema
+from . import client
 
 from .test_dataupload import test_data_upload_markdown
 from .test_dataupload import test_data_upload_processed_sentences
@@ -41,11 +41,6 @@ def test_chat_websocket_connection(mocker, fresh_db):
     mocker.patch("app.routers.Supabase.get_accessible_labels", return_value=["mock-label"])
     mocker.patch("app.core.llm_framework.openai_vanilla.ChatCompletion.create",
                             return_value={"choices":[{"message":{"content":"Mock response"}}]})
-    # mocker.patch("app.routers.ConversationPipeline.llm_framework.generate_text",
-    #                                                     return_value={"question": "Mock Question",
-    #                                                         "answer": "Mock answer",
-    #                                                         "source_documents": [],
-    #                                                         })
     args = COMMON_CONNECTION_ARGS.copy()
     args["dbPath"] = fresh_db["dbPath"]
     args["collectionName"] = fresh_db["collectionName"]
@@ -56,15 +51,13 @@ def test_chat_websocket_connection(mocker, fresh_db):
         assert "Mock" in data['message']
 
 @pytest.mark.with_llm
-def test_chat_based_on_translationwords_with_llm(mocker, fresh_db):
+def test_chat_based_on_data_in_db(mocker, fresh_db):
     """Add some docs and ask questions on it, with default configs"""
     mocker.patch("app.routers.Supabase.check_token",
                  return_value={"user_id": "1111"})
     mocker.patch("app.routers.Supabase.check_role", return_value=True)
     mocker.patch("app.routers.Supabase.get_accessible_labels",
                         return_value=COMMON_CONNECTION_ARGS['labels'])
-    # mocker.patch("app.core.llm_framework.openai_vanilla.ChatCompletion.create",
-    #                         return_value={"choices":[{"message":{"content":"Mock response"}}]})
     args = COMMON_CONNECTION_ARGS.copy()
     args["dbPath"] = fresh_db["dbPath"]
     args["collectionName"] = fresh_db["collectionName"]
@@ -78,14 +71,16 @@ def test_chat_based_on_translationwords_with_llm(mocker, fresh_db):
         data = websocket.receive_json()
         assert_positive_bot_response(data)
 
-        websocket.send_bytes(json.dumps({"message":"Can you tell me about angels?"}).encode('utf-8'))
+        websocket.send_bytes(
+            json.dumps({"message":"Can you tell me about angels?"}).encode('utf-8'))
         data = websocket.receive_json()
         assert_positive_bot_response(data)
         assert "angel" in data["message"].lower()
         assert "spirit" in data["message"].lower()
         assert "god" in data["message"].lower()
 
-        # websocket.send_bytes(json.dumps({"message":"How was earth in the beginning?"}).encode('utf-8'))
+        # websocket.send_bytes(
+        #    json.dumps({"message":"How was earth in the beginning?"}).encode('utf-8'))
         # data = websocket.receive_json()
         # assert_positive_bot_response(data)
         # assert "heaven" in data["message"].lower()
