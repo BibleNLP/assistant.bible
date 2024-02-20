a3b1ca9fcbc062d5f42ba005c0f24365edd5eb6b
aquintero <alejandro_quintero@sil.org>
2024-02-20 16:54:38 -0700

    using aws translator instead of gcp

--- app/core/translation/__init__.py
+++ app/core/translation/__init__.py
@@ -1,26 +1,33 @@
 import json
 
-from google.cloud import translate
+import boto3
 
-
-with open('../iso639-1.json') as f:
-    iso_639_1 = json.load(f)
+# with open('../iso639-1.json') as f:
+#     iso_639_1 = json.load(f)
 
 
 def translate_text(text:str):
-    PROJECT_ID = 'studied-flow-385100'
-    PARENT = f'projects/{PROJECT_ID}'
-    TRANSLATE = translate.TranslationServiceClient()
-    data = {
-                'contents': [text],
-                'parent': PARENT,
-                'target_language_code': 'en-US',
-            }
+    translate = boto3.client(service_name='translate', use_ssl=True)
     try:
-        response = TRANSLATE.translate_text(request=data)
-    except TypeError:
-        response = TRANSLATE.translate_text(**data)
+        # Call AWS Translate
+        response = translate.translate_text(
+            Text=text,
+            SourceLanguageCode='auto',  # Automatically detect the source language
+            TargetLanguageCode='en'     # Target language code is 'en' for English
+        )
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return {}
     
-    response['language'] = iso_639_1.get(response.translations[0].detected_language_code)
+    source_language_code = response.get('SourceLanguageCode', '').split('-')[0]  # Extracting ISO 639-1 code part
+    # response['language'] = iso_639_1.get(source_language_code, "Unknown language")
 
     return response
+
+if __name__ == "__main__":
+    # Example text to translate
+    test_text = "Hola, mundo"
+    # Call the translate_text function
+    result = translate_text(test_text)
+    # Print the result
+    print(result)
