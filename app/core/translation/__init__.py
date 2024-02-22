d6ea04b07cfd26095041d741cab3c77a3e588834
Mark <woodwardmw@gmail.com>
2024-02-21 22:01:40 -0800

    Fix most linting issues

--- app/core/translation/__init__.py
+++ app/core/translation/__init__.py
@@ -1,33 +1,43 @@
+"""Translating the user's input to English using AWS Translate service"""
+
 import json
 
 import boto3
 
-with open('../iso639-1.json') as f:
+with open("../iso639-1.json", "r", encoding="utf-8") as f:
     iso_639_1 = json.load(f)
 
 
-def translate_text(text:str):
-    translate = boto3.client(service_name='translate', use_ssl=True)
+def translate_text(text: str):
+    """
+    Translates the given text to English using AWS Translate service
+    :param text: The text to translate
+    :return: The translated text
+    """
+    translate = boto3.client(service_name="translate", use_ssl=True)
     try:
         # Call AWS Translate
         response = translate.translate_text(
             Text=text,
-            SourceLanguageCode='auto',  # Automatically detect the source language
-            TargetLanguageCode='en'     # Target language code is 'en' for English
+            SourceLanguageCode="auto",  # Automatically detect the source language
+            TargetLanguageCode="en",  # Target language code is 'en' for English
         )
     except Exception as e:
         print(f"An error occurred: {e}")
         return {}
-    
-    source_language_code = response.get('SourceLanguageCode', '').split('-')[0]  # Extracting ISO 639-1 code part
-    response['language'] = iso_639_1.get(source_language_code, "Unknown language")
+
+    source_language_code = response.get("SourceLanguageCode", "").split("-")[
+        0
+    ]  # Extracting ISO 639-1 code part
+    response["language"] = iso_639_1.get(source_language_code, "Unknown language")
 
     return response
 
+
 if __name__ == "__main__":
     # Example text to translate
-    test_text = "Hola, mundo"
+    TEST_TEXT = "Hola, mundo"
     # Call the translate_text function
-    result = translate_text(test_text)
+    result = translate_text(TEST_TEXT)
     # Print the result
     print(result)
